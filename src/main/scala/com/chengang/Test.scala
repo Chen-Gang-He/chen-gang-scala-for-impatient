f0cbe430158f76138b71a213494bce3296a42291
Chen-Gang <che@merkeinc.com>
2018-09-22 15:07:15 +0800

    Initial commit

---
+++ src/main/scala/com/chengang/Test.scala
@@ -0,0 +1,9 @@
+package com.chengang
+
+/**
+  * Created by che on 9/22/2018.
+  */
+object Test extends App{
+  println("Test")
+
+}
---
+++ src/scala/Test.scala
@@ -0,0 +1,8 @@
+/**
+  * Created by che on 9/22/2018.
+  */
+object Test extends App {
+
+  println("Test")
+
+}
